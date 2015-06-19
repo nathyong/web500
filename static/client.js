6510f92a17d891561c65b6702bf65e66b97a8375
Vince Au <vinceau09@gmail.com>
2015-06-19 18:05:38 +1000

    Allow client to authenticate using secret keys

--- static/client.js
+++ static/client.js
@@ -1,13 +1,38 @@
 var ws = new WebSocket("ws://" + location.host + "/socket");
 ws.onopen = function() {
-    //ws.send("Hello, world");
+    ws.send(JSON.stringify({act : 'auth'}));
 };
 ws.onmessage = function(evt) {
     data = JSON.parse(evt.data);
     if (data.act === 'chat') {
         $('#msglist tr:last').after('<tr><td>' + data.from + ':</td><td>' + data.message + '</td></tr>');
     }
+    else if (data.act === 'auth') {
+        if (data.response === 'success') {
+            $('#username').val(data.username);
+            $('#secretkey').html(data.secretkey);
+            $('#error').hide();
+        } else if (data.response === 'taken') {
+            //prompt for login or new username
+            $('#username').val(data.username);
+            $('#error, #login').show();
+        } else {
+            //unsuitable user name (anonymous)
+            $('#username').val(data.username);
+            $('#error').show();
+        }
+    }
 };
+
+function setuser(name, key) {
+    key = typeof key !== 'undefined' ? key : '';
+    data = {
+        act : 'auth',
+        username : name,
+        secretkey : key,
+    }
+    ws.send(JSON.stringify(data));
+}
 
 function sendmsg() {
     data = {
@@ -17,3 +42,17 @@ function sendmsg() {
     ws.send(JSON.stringify(data));
     $('#msgbox').val('');
 }
+
+$("#keyprompt").keypress(function(event) {
+    if (event.which == 13) {
+        setuser($('#username').val(), $(this).val());
+    }
+});
+
+$("#username").keypress(function(event) {
+    if (event.which == 13) {
+        setuser($(this).val());
+    }
+});
+
+
