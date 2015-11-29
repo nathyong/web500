38da132bfbe38b372f72a53123f64e07a0376bb1
nathyong <nathyong@gmail.com>
2015-11-30 10:25:42 +1100

    Initialise database if it doesn't exist

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ from tornado.websocket import WebSocketHandler
 from tornado.wsgi import WSGIContainer
 
 import web500
+from web500.db import get_db, query_db
 
 options.define("port",
                default=8888,
@@ -33,7 +34,16 @@ def main():
          (r"/game/ws", GameSocketHandler),
          (r".*", FallbackHandler, dict(fallback=WSGIContainer(web500.app)))])
     application.listen(options.options.port)
-    logging.getLogger("tornado.general").info("Server is starting on port {}".format(options.options.port))
+    logger = logging.getLogger("tornado.general")
+    with web500.app.app_context():
+        db = get_db()
+        if not query_db("""SELECT * FROM sqlite_master
+                        WHERE name ='users' and type='table';"""):
+            logger.info("Initialising database")
+            with web500.app.open_resource('schema.sql', mode='r') as f:
+                db.cursor().executescript(f.read())
+            db.commit()
+    logger.info("Server is starting on port {}".format(options.options.port))
     tornado.ioloop.IOLoop.instance().start()
 
 
