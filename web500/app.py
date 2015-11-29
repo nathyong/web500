5f6965b7d63558cd59eaab66797cf4eddf505c29
nathyong <nathyong@gmail.com>
2015-11-29 22:29:31 +1100

    Allow users to log in and log out

--- web500/app.py
+++ web500/app.py
@@ -1,14 +1,28 @@
 """Application and presentation logic for web500.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 import web500.config
 
 app = Flask(__name__)
-app.settings = web500.config.settings
+app.secret_key = web500.config.settings['secret_key']
 
 
 @app.route('/')
 def index():
     """Handles the index page"""
+    if 'username' not in session:
+        return render_template("login.html")
     return render_template("index.html")
+
+@app.route('/login', methods=["POST"])
+def login():
+    """Authenticates new users"""
+    session['username'] = request.form['username']
+    return redirect(url_for("index"))
+
+@app.route('/logout')
+def logout():
+    """Unsets the currently logged-in session"""
+    session.pop("username", None)
+    return redirect(url_for("index"))
