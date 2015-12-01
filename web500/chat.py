568f6c17f3a1d488199372fe973a1d470769a3a8
nathyong <nathyong@gmail.com>
2015-12-01 11:16:22 +1100

    Show a list of connected users on chat page (#3)
    The structure of the ChatSocketHandler has changed somewhat and web
    styles have been modified in anticipation of future styling.  The
    ChatSocketHandler now utilises Flask's session cookies.

--- web500/chat.py
+++ web500/chat.py
@@ -11,6 +11,9 @@ from itsdangerous import URLSafeTimedSerializer
 from web500.app import app
 
 
+flaskSessionInterface = flask.sessions.SecureCookieSessionInterface()
+cookieSerializer = flaskSessionInterface.get_signing_serializer(app)
+
 def total_seconds(td):
     """Returns the total seconds from a timedelta object.  Copied from
     ``flask.helpers``.
@@ -23,32 +26,46 @@ class ChatSocketHandler(WebSocketHandler):
 
     Cookies from the corresponding Flask session are inspected during the
     initial handshake, and sessions with invalid cookies are dropped.
+
+    .. attribute:: connected_sessions
+
+       A dictionary of format {user: socket}, used to represent the users who
+       are connected to the chat server.
     """
 
-    sockets_list = []
+    connected_sessions = {}
+
+    def send_user_list(self):
+        """Sends the list of users to all connected users.
+        """
+        user_list_response = {"act": "users",
+                              "users": list(self.connected_sessions.keys())}
+        for sock in self.connected_sessions.values():
+            sock.write_message(user_list_response)
+
 
     def get_flask_session(self):
         """Unencrypts a Flask encrypted session cookie.
 
         Based on code in ``flask.sessions``.
         """
-        flaskSessionInterface = flask.sessions.SecureCookieSessionInterface()
-        cookieSerializer = flaskSessionInterface.get_signing_serializer(app)
         cookie = self.get_cookie(app.session_cookie_name)
         max_age = total_seconds(app.permanent_session_lifetime)
         return cookieSerializer.loads(cookie, max_age=max_age)
 
     @tornado.web.asynchronous
     def get(self, *args, **kwargs):
-        cookie_data = self.get_flask_session()
-        if "username" not in cookie_data:
+        session = self.get_flask_session()
+        if "username" not in session:
             app.logger.warning("chat: unauthorised user tried to connect to chat")
             return
         super(ChatSocketHandler, self).get(*args, **kwargs)
 
     def open(self):
-        self.sockets_list.append(self)
+        session = self.get_flask_session()
+        self.connected_sessions[session["username"]] = self
         app.logger.info("chat: socket connection opened")
+        self.send_user_list()
 
     def on_message(self, message):
         contents = json.loads(message)
@@ -56,9 +73,11 @@ class ChatSocketHandler(WebSocketHandler):
         response = {"act": "chat",
                     "from": session["username"],
                     "message": Markup.escape(contents["message"])}
-        for sock in self.sockets_list:
+        for sock in self.connected_sessions.values():
             sock.write_message(response)
 
     def on_close(self):
-        self.sockets_list.remove(self)
+        session = self.get_flask_session()
+        del self.connected_sessions[session["username"]]
         app.logger.info("chat: socket connection closed")
+        self.send_user_list()
--- web500/static/client.js
+++ web500/static/client.js
@@ -1,7 +1,25 @@
 var ws = new WebSocket("ws://" + location.host + "/chat/ws");
+
+ws.onopen = function() {
+    $('#msglist').append('<tr class="chatline"><td class="prefix">—</td><td class="message">Connected to chat server</td></tr>');
+};
+
 ws.onmessage = function(evt) {
     data = JSON.parse(evt.data);
-    $('#msglist tr:last').after('<tr><td>' + data.from + ':</td><td>' + data.message + '</td></tr>');
+    switch (data.act) {
+        case 'chat':
+            $('#msglist tr:last').after('<tr class="chatline"><td class="prefix">' + data.from + ':</td><td class="message">' + data.message + '</td></tr>');
+            break;
+        case 'users':
+            $('#onlineusers ul').empty();
+            for (var user of data.users) {
+                $('#onlineusers ul').append('<li>' + user + '</li>');
+            }
+            break;
+        default:
+            $('#msglist tr:last').after('<tr class="chatline chatline-important"><td class="prefix">DEBUG:</td><td class="message">' + evt.data + '</td></tr>');
+            break;
+    }
 };
 
 function sendmsg() {
