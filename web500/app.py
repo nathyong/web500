3f46541cd249ef98b680cf20c08754fb6aecedd1
Vince Au <vinceau09@gmail.com>
2016-01-08 17:25:33 +1100

    Automatically compile scss on run

--- web500/app.py
+++ web500/app.py
@@ -2,8 +2,14 @@
 """
 
 from flask import Flask
+from web500.gen_css import convert_dir
+from os.path import dirname, join
 
 app = Flask(__name__)
+
+#convert all scss files in scss/ folder to css
+cd = dirname(__file__) #current directory
+convert_dir(join(cd, 'scss'), join(cd, 'static/css/gen'))
 
 import web500.config
 import web500.routes
